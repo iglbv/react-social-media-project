c5f2ef843adf1d657940d1e9ec1948796b36b993
iglbv <ilyagolubev2021@gmail.com>
2026-01-17 00:02:34 +0500

    fix useEffect() error and add loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import PostService from "./API/PostService";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -14,10 +15,11 @@ function App() {
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
   useEffect(() => {
     fetchPosts()
-  }, [filter])
+  }, [])
 
   function createPost(newPost) {
     setPosts([...posts, newPost])
@@ -25,8 +27,10 @@ function App() {
   }
 
   async function fetchPosts() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    setPosts(response.data)
+    setIsPostsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts)
+    setIsPostsLoading(false);
   }
 
   function removePost(post) {
@@ -47,7 +51,10 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
-      {
+      {isPostsLoading
+        ?
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div>
+        :
         <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
       }
     </div >
